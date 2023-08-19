48c13a04c8e643381f5bbb82e2aa28d5e3671962
SABARINATH P S <sabarinath.p@flycatchtech.com>
2023-08-19 10:49:41 +0530

    Folder structure defined

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
